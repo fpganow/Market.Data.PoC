8343f5d6523946b6c4ad15804dfdf26a3fc5f564
Fpga John <john@fpganow.com>
2025-08-01 21:21:41 -0400

    Updating design, leaving out dcp file. Will it still work?

--- vivado/kr260/poc_kr260/my_bds/design_1/hdl/design_1_wrapper.v
+++ vivado/kr260/poc_kr260/my_bds/design_1/hdl/design_1_wrapper.v
@@ -2,8 +2,8 @@
 //Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
-//Date        : Thu Jun 26 13:43:36 2025
-//Host        : ASUS running 64-bit major release  (build 9200)
+//Date        : Thu Jul 31 16:06:21 2025
+//Host        : Ryzen10 running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
 //Purpose     : IP block netlist
@@ -11,11 +11,71 @@
 `timescale 1 ps / 1 ps
 
 module design_1_wrapper
-   (fan_en_b);
+   (fan_en_b,
+    gt_ref_clk_clk_n,
+    gt_ref_clk_clk_p,
+    gt_rtl_grx_n,
+    gt_rtl_grx_p,
+    gt_rtl_gtx_n,
+    gt_rtl_gtx_p,
+    sfp_iic_scl_io,
+    sfp_iic_sda_io,
+    sfp_led_tri_o,
+    sfp_tx_dis);
   output [0:0]fan_en_b;
+  input gt_ref_clk_clk_n;
+  input gt_ref_clk_clk_p;
+  input [0:0]gt_rtl_grx_n;
+  input [0:0]gt_rtl_grx_p;
+  output [0:0]gt_rtl_gtx_n;
+  output [0:0]gt_rtl_gtx_p;
+  inout sfp_iic_scl_io;
+  inout sfp_iic_sda_io;
+  output [1:0]sfp_led_tri_o;
+  output [0:0]sfp_tx_dis;
 
   wire [0:0]fan_en_b;
+  wire gt_ref_clk_clk_n;
+  wire gt_ref_clk_clk_p;
+  wire [0:0]gt_rtl_grx_n;
+  wire [0:0]gt_rtl_grx_p;
+  wire [0:0]gt_rtl_gtx_n;
+  wire [0:0]gt_rtl_gtx_p;
+  wire sfp_iic_scl_i;
+  wire sfp_iic_scl_io;
+  wire sfp_iic_scl_o;
+  wire sfp_iic_scl_t;
+  wire sfp_iic_sda_i;
+  wire sfp_iic_sda_io;
+  wire sfp_iic_sda_o;
+  wire sfp_iic_sda_t;
+  wire [1:0]sfp_led_tri_o;
+  wire [0:0]sfp_tx_dis;
 
   design_1 design_1_i
-       (.fan_en_b(fan_en_b));
+       (.fan_en_b(fan_en_b),
+        .gt_ref_clk_clk_n(gt_ref_clk_clk_n),
+        .gt_ref_clk_clk_p(gt_ref_clk_clk_p),
+        .gt_rtl_grx_n(gt_rtl_grx_n),
+        .gt_rtl_grx_p(gt_rtl_grx_p),
+        .gt_rtl_gtx_n(gt_rtl_gtx_n),
+        .gt_rtl_gtx_p(gt_rtl_gtx_p),
+        .sfp_iic_scl_i(sfp_iic_scl_i),
+        .sfp_iic_scl_o(sfp_iic_scl_o),
+        .sfp_iic_scl_t(sfp_iic_scl_t),
+        .sfp_iic_sda_i(sfp_iic_sda_i),
+        .sfp_iic_sda_o(sfp_iic_sda_o),
+        .sfp_iic_sda_t(sfp_iic_sda_t),
+        .sfp_led_tri_o(sfp_led_tri_o),
+        .sfp_tx_dis(sfp_tx_dis));
+  IOBUF sfp_iic_scl_iobuf
+       (.I(sfp_iic_scl_o),
+        .IO(sfp_iic_scl_io),
+        .O(sfp_iic_scl_i),
+        .T(sfp_iic_scl_t));
+  IOBUF sfp_iic_sda_iobuf
+       (.I(sfp_iic_sda_o),
+        .IO(sfp_iic_sda_io),
+        .O(sfp_iic_sda_i),
+        .T(sfp_iic_sda_t));
 endmodule
--- vivado/kr260/poc_kr260/my_cons/cons.xdc
+++ vivado/kr260/poc_kr260/my_cons/cons.xdc
@@ -8,3 +8,28 @@ set_property PACKAGE_PIN A12 [get_ports {fan_en_b}]
 set_property IOSTANDARD LVCMOS33 [get_ports {fan_en_b}]
 set_property SLEW SLOW [get_ports {fan_en_b}]
 set_property DRIVE 4 [get_ports {fan_en_b}]
+
+# GTH pins
+set_property PACKAGE_PIN Y6 [get_ports gt_ref_clk_clk_p]
+set_property PACKAGE_PIN T2 [get_ports gt_rtl_grx_p]
+set_property PACKAGE_PIN R4 [get_ports gt_rtl_gtx_p]
+
+# sfp pins
+set_property IOSTANDARD LVCMOS33 [get_ports {sfp_tx_dis[0]}]
+set_property PACKAGE_PIN Y10 [get_ports {sfp_tx_dis[0]}]
+set_property IOSTANDARD LVCMOS18 [get_ports {sfp_led_tri_o[0]}]
+set_property PACKAGE_PIN G8 [get_ports {sfp_led_tri_o[0]}]
+set_property IOSTANDARD LVCMOS18 [get_ports {sfp_led_tri_o[1]}]
+set_property PACKAGE_PIN F7 [get_ports {sfp_led_tri_o[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {sfp_iic_scl_io}]
+set_property PACKAGE_PIN AB11 [get_ports {sfp_iic_scl_io}]
+set_property IOSTANDARD LVCMOS33 [get_ports {sfp_iic_sda_io}]
+set_property PACKAGE_PIN AC11 [get_ports {sfp_iic_sda_io}]
+
+# timing contraints
+set_false_path -to [get_ports {sfp_iic_scl_io*}]
+set_false_path -to [get_ports {sfp_iic_sda_io*}]
+set_false_path -from [get_ports {sfp_iic_scl_io*}]
+set_false_path -from [get_ports {sfp_iic_sda_io*}]
+
+set_false_path -to [get_ports {sfp_led_tri_o*}]
